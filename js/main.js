d516d531b79c494099562b10cd09ffd20630b311
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2023-01-11 14:06:30 +0300

    add many style: hover and disabled

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log('Hello world');
+
+function init(){
+    let map = new ymaps.Map('map-body', {
+        center: [55.75399399999374,37.62209300000001],
+        zoom: 13
+    });
+
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+    map.behaviors.disable(['scrollZoom']);
+}
+
+ymaps.ready(init)
